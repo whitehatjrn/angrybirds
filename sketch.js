fce3eff61856fb9a4c35d67e5cabfd34b33a82a4
whitehatjrn <whitehatjrn@gmail.com>
2020-12-19 15:50:51 +0530

    back to prev

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1, platform, catapult;
+var box1, pig1;
 var backgroundImg;
 
 function preload() {
@@ -16,9 +16,7 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-    platform= new Ground(150,305,300,170);
-    catapult= new Ground(205,190,30,70);
+    ground = new Ground(600,height,1200,20);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -61,6 +59,4 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
-    catapult.display();
 }
